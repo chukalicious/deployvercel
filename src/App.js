7973071922bcf833c1fab410a8259c02d73576be
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-08-03 02:16:35 -0700

    simple button fucntionaly for my deploy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+  console.log(message);
+
+  const handleClick = () => {
+    setMessage("Lift off!!");
+  };
+  const changeMessage = () => {
+    setMessage("Back to earth");
+  };
   return (
     <div className="App">
-      <h2>my deployment</h2>
+      <h2>My Deployment</h2>
+      <div>
+        <button style={{ marginBottom: "1rem" }} onClick={handleClick}>
+          Deploy! 🚀
+        </button>
+      </div>
+
+      <button style={{ marginBottom: "1rem" }} onClick={changeMessage}>
+        Land 🌎{" "}
+      </button>
+
+      <div>
+        <p>{message}</p>
+      </div>
     </div>
   );
 }
